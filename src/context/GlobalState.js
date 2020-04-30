8a764b4e4ab4254aa70b0319d2ee94f6cf2c37a4
Luke Russell <thelukerussel21@gmail.com>
2020-04-30 15:08:23 -0400

    functionality

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -3,12 +3,7 @@ import AppReducer from './AppReducer';
 
 //initial state
 const initialState = {
-  transactions: [
-    { id: 1, text: 'Flower', amount: -20 },
-    { id: 2, text: 'Salary', amount: 300 },
-    { id: 3, text: 'Book', amount: -10 },
-    { id: 4, text: 'Camera', amount: 150 },
-  ],
+  transactions: [],
 };
 
 // create context
@@ -18,8 +13,19 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
+  // actions
+  const deleteTransaction = (id) => {
+    dispatch({ type: 'delete_transaction', payload: id });
+  };
+
+  const addTransaction = (transaction) => {
+    dispatch({ type: 'add_transaction', payload: transaction });
+  };
+
   return (
-    <GlobalContext.Provider value={{ transactions: state.transactions }}>
+    <GlobalContext.Provider
+      value={{ transactions: state.transactions, deleteTransaction, addTransaction }}
+    >
       {children}
     </GlobalContext.Provider>
   );
