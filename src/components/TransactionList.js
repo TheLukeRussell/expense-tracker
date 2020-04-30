6b47cab84ccdc4c936a86f4b9443784210137613
Luke Russell <thelukerussel21@gmail.com>
2020-04-30 14:49:32 -0400

    amounts rendering on page

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,16 +1,15 @@
 import React, { useContext } from 'react';
-import GlobalContext from '../context/GlobalState';
+import { GlobalContext } from '../context/GlobalState';
 import Transaction from './Transaction';
 
 const TransactionList = () => {
   const { transactions } = useContext(GlobalContext);
-
   return (
     <>
       <h3>History</h3>
       <ul className='list'>
         {transactions.map((transaction) => (
-          <Transaction key={transaction.id} transaction={transactions} />
+          <Transaction key={transaction.id} transaction={transaction} />
         ))}
       </ul>
     </>
